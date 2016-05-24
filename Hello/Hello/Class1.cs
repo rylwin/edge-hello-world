6219ba9c62093fc4c9782b12b10f3b906bf63efb
Ryan Winograd <ryan@thewinograds.com>
2016-05-24 11:55:09 -0500

    Add Hello c# project

---
+++ Hello/Hello/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Hello
+    {
+        public async Task<object> SayHello(object _)
+        {
+            Console.WriteLine("Hello!");
+            return await Task.FromResult<object>(null);
+        }
+    }
+}
